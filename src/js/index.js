b49e32de9088546c8fb77d13dd24b008b90cd3c6
Phil Paolo <philipcpaolo@gmail.com>
2020-08-31 11:47:07 -0400

    install/configure babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import test from './test';
-
-console.log(`I imported ${test} from another module!`);
+const x = 23;
+console.log(`I imported ${test} from another module! variable x is ${x}.`);
--- webpack.config.js
+++ webpack.config.js
@@ -15,5 +15,17 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         })
-    ]
+    ],
+    // babel setup
+    module: {
+        rules: [ 
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
 };
