3fe66672c8cfe0dcafffb1816f5cc822eb31f9ad
Phil Paolo <philipcpaolo@gmail.com>
2020-09-01 10:39:58 -0400

    set up API call and search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,35 @@
-// Global app controller
-import test from './test';
-const x = 23;
-console.log(`I imported ${test} from another module! variable x is ${x}.`);
+import Search from './models/Search';
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async()=> {
+    // 1. Get query from view
+    const query = 'pizza'; //TODO
+
+    if(query) {
+        // 2. New search object and add to state
+        state.search = new Search(query);
+    }
+    // 3. Prepare UI for results
+    
+    // 4. Search for recipes
+    await state.search.getResults();
+
+    // 5. Render results on UI
+    console.log(state.search.results);
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
+//const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+
