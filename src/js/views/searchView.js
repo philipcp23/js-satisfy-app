926e0b7fe85774b9ea4cd29b45dd7e1756481976
Phil Paolo <philipcpaolo@gmail.com>
2020-09-03 13:40:59 -0400

    update recipe servings

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -15,9 +15,8 @@ export const highlightSelected = id => {
     const resultsArr = Array.from(document.querySelectorAll('.results__link'));
     resultsArr.forEach(el => {
         el.classList.remove('results__link--active');
-    })
-    
-    document.querySelectorAll(`a[href*="#${id}"]`).classList.add('results__link--active');
+    });    
+    document.querySelector(`.results__link[href*="${id}"]`).classList.add('results__link--active');
 };
 
 // 'Pasta wih tomato and spinach'
